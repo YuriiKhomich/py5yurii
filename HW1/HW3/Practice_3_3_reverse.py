a373cdc4a59ab62725d43e20e8b1ae6630586f5d
YuriiKhomich <123492828+YuriiKhomich@users.noreply.github.com>
2023-01-25 09:53:46 +0200

    Delete HW1 directory

--- HW1/HW3/Practice_3_3_reverse.py
+++
@@ -1,8 +0,0 @@
-#Написать программу, которая выводит саму себя задом наперед
-f = open('Practice_3_3_reverse.py', encoding='utf-8-sig')
-spisok = f.read().rstrip().split('\n')
-spisok = spisok[::-1]
-for line in spisok:
-    print(line[::-1].lstrip())
-f.close()
-
--- HW1/HW5/HW_5_3_self_ZIP.py
+++
@@ -1,8 +0,0 @@
-# HomeWork 5.3
-# Знайомимось з zip функцією, пробуємо написати свій власний zip
-
-
-my_numbers = [1, 2, 3, 4, 5]
-my_list = ['Manowar', 'ACDC', 'ACCEPT']
-results = list(map(lambda x, y: (x, y), my_numbers, my_list))
-print(results)
